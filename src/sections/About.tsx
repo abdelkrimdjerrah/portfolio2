7479a3b4e53776f713b1d093a19fe461f7c128bb
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-07-06 00:37:17 +0100

    about

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,11 +1,7 @@
 export const About = () => {
   return (
-    // <div className="text-white w-full flex items-center justify-center gap-20 py-64">
-    // <img className="w-[800px] abdelkrim" src={abdelkrim_main} alt="" />
-    //   <div className="">I'm a MERN stack web developer specializing in front-end develpment, with a passion for designing in UI/UX and brand design of what’s different, new, and unique with my own creativity. I seek to provide innovative solutions, from concept creations to final executions, with creativity, quality, and simplicity. I'm experienced, independent, and highly motivated, and I'm always looking to take on new challenges and provide the best possible results. Feel free to contact me with any questions or to discuss your next project.</div>
-    // </div>
-    <div>
-        
+    <div className="text-[#a0ecff] flex items-center justify-center w-full px-20 lg:px-28 py-64 mt-28 lg:mt-0">
+      <div className="text-center text-3xl font-light leading-[42px]">I'm a MERN stack web developer specializing in front-end develpment, with a passion for designing in UI/UX and brand design of what’s different, new, and unique with my own creativity. I seek to provide innovative solutions, from concept creations to final executions, with creativity, quality, and simplicity. I'm experienced, independent, and highly motivated, and I'm always looking to take on new challenges and provide the best possible results. Feel free to contact me with any questions or to discuss your next project.</div>
     </div>
   );
 };
