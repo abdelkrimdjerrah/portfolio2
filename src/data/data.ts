d44271f17f160cbb5910aad4e4c051b37e6f3add
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-07-23 00:06:52 +0100

    changes

--- src/data/data.ts
+++ src/data/data.ts
@@ -17,7 +17,7 @@ import {
   IconBehance,
 } from "../icons/Icons";
 
-export const about = "I'm a MERN stack web developer specializing in front-end development, with a passion for designing in UI/UX and brand design of what’s different, new, and unique with my own creativity. I seek to provide innovative solutions, from concept creations to final executions, with creativity, quality, and simplicity. I'm experienced, independent, and highly motivated, and I'm always looking to take on new challenges and provide the best possible results. Feel free to contact me with any questions or to discuss your next project."
+export const about = "I'm a MERN stack web developer specializing in front-end development, with a passion for designing in UI/UX and brand design of what’s different, new, and unique with my own creativity. I seek to provide innovative solutions, from concept creations to final executions, with creativity, quality, and simplicity. I'm experienced, independent, and highly motivated, and I'm always looking to take on new challenges and provide the best possible results."
 
 export const skills = [
   {
